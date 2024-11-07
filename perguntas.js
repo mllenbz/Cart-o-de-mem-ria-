18a2cb28f57c79a3552a3c64e688b2923bc3892f
mllenbz <millenab886@gmail.com>
2024-11-07 10:39:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Chárada',
+    'O que é, o que é? Feito para andar e não anda?',
+    'A rua'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos EUA é Washington'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 4x7?',
+    '28'
+)
+
+criaCartao(
+    'Lingua castelhana',
+    'Como se diz bom dia em espanhol?',
+    'Buen Día'
+)
